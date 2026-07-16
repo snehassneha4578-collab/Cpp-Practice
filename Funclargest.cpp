de9dc4510926f55b239ea3c2412b0cd562356bfe
Sneha S <snehassneha4578@gmail.com>
2026-07-16 09:00:25 +0530

    Add files via upload
    Largest number

---
+++ Funclargest.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int largest(int a,int b)
+{
+
+if(a>b)
+    return a;
+else
+    return b;
+
+}
+
+int main()
+{
+
+cout<<largest(10,20);
+
+return 0;
+
+}
