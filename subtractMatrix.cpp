16a2e297534de3bdb9d7e14fca8047258abb1b99
Sneha S <snehassneha4578@gmail.com>
2026-07-18 08:10:33 +0530

    Add files via upload

---
+++ subtractMatrix.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[2][2], b[2][2], c[2][2];
+
+    cout<<"Enter first matrix:"<<endl;
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"Enter second matrix:"<<endl;
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+
+    cout<<"Subtraction Matrix:"<<endl;
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
