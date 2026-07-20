f55362ea8680d52804ae9d1298f533102c1f7ef6
Sneha S <snehassneha4578@gmail.com>
2026-07-20 06:32:13 +0530

    Add files via upload

---
+++ PtrArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[5]={10,20,30,40,50};
+    int *ptr=a;
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<*(ptr+i)<<" ";
+    }
+
+    return 0;
+}
